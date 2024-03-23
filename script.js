316bc5b0cce78c1deca9e8ce2281ceed4466513d
Mohammed-Rinshad <154004738+Mohammed-Rinshad@users.noreply.github.com>
2024-03-23 10:43:46 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let intvalue = 0;
 let rsum = 0;
 let Rscore = document.getElementById('Score').value;
+let disableOpplick = true;
+let out = document.getElementById('out');
+let defeat = document.getElementById('defeat')
+let won = document.getElementById('won')
+let data;
+
 
 function handleClick(numToAdd) {
 
@@ -13,62 +19,46 @@ function handleClick(numToAdd) {
   const max = 6;
   let randomNum;
   randomNum = Math.floor(Math.random() * max) + min;
-  let out = document.getElementById('gameover');
   if (numToAdd === randomNum) {
+    //let out = document.getElementById('out');
     out.textContent = 'OUT';
     setTimeout(() => {
       out.style.display = "none";
     }, 3000);
     handleClick = null
+    disableOpplick = false;
+    data = document.getElementById("score").value
+    
+    
   }
 }
+    
     function oppClick(num){
+    if (disableOpplick) return;
     const Min = 1; 
     const Max = 6; 
-    let Randomnum; 
-    Randomnum = Math.floor(Math.random() * Max) + Min;
-    rsum += Randomnum 
+    let randomnum; 
+    randomnum = Math.floor(Math.random() * Max) + Min;
+    rsum += randomnum 
     Rscore = parseInt(rsum);
     document.getElementById("Score").value = rsum;
-    console.log(Randomnum)
-    let out = document.getElementById('gameover');
-    if (Randomnum === num){
-      out.textContent = 'OUT';
+    //console.log(randomnum)
+    //let out = document.getElementById('out');
+    if (Rscore > data){
+      disableOpplick = true;
+      defeat.textContent = 'OPPONENT WINS';
       setTimeout(() => {
-        out.style.display = "none";
+        defeat.style.display = "none";
+      }, 3000);
+    }else if(num === randomnum && Rscore < data){
+      disableOpplick = true;
+      won.textContent = 'YOU WON';
+      setTimeout(() => {
+        won.style.display = "none";
       }, 3000);
     }
+
     }
 
 
 
-
-
-
-//if(randomNum === numToAdd){
-    //function handleClickOpponent(randomToAdd){
-    //Rsum = randomnum
-    //document.getElementById("Score").value = Rsum
-    //const Min = 1;
-    //const Max = 6;
-    //let randomnum;
-    //randomnum = Math.floor(Math.random() *Max) + Min
-    //console.log(randomnum)
-    //Rscore = String(Rsum)
-
-  //}
-//}
-
-
-//let btn = document.getElementById('button')
-//
-//btn.onclick = function Click(btn1,btn2, btn3, btn4, btn5){
-//  btn1 = 1
-//  btn2 = 2
-//  btn3 = 3
-//  btn4 = 4
-//  btn5 = 5
-//
-//  
-//
-//}
